0b86ab26c2af4f098df976a82027bd481e5989f2
Amy Wah <amyspod@gmail.com>
2022-10-14 18:22:52 -0400

    added a test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,4 +5,8 @@ describe('Presentation Title', () => {
   it('has the correct title', () => {
     cy.get('[data-cy="title"]').contains("End To End Testing");
   })
+  it('moves to the next slide when the right arrow is pressed', () => {
+    cy.get('body').type('{rightArrow}');
+    cy.url().should('contain', 'slideIndex=1&stepIndex=0');
+  })
 })
