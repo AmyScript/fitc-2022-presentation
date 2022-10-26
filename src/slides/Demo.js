e029ab963f45fed47a6b4dfa72fa17291bc8453e
Amy Wah <amyspod@gmail.com>
2022-10-25 20:20:49 -0400

    demo notes'

--- src/slides/Demo.js
+++ src/slides/Demo.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
     Heading,
     Slide,
-    FlexBox
+    FlexBox,
+    Notes
   } from 'spectacle';
 
 import Milo from '../images/Milo.jpg';
@@ -18,11 +19,17 @@ import Milo from '../images/Milo.jpg';
         <Slide>
             <FlexBox data-cy="demo-slide" flexDirection="column">
                 <Heading>Demo</Heading>
+                <button data-cy="get-image-button" onClick={getImage}>SHOW MILO</button>
                 <FlexBox width="100%" flexDirection="row" alignItems="center">
-                    <button data-cy="get-image-button" onClick={getImage}>SHOW MILO</button>
                     {showImage ? (<img width="30%" data-cy="milo-image" src={Milo} />) : null}
                 </FlexBox>
             </FlexBox>
+            <Notes>
+                We will now give a quick live demo of running some e2e tests using Cypress.  Our presentation slides were
+                developed using a React library called spectacle and so we will perform some e2e tests on our presentation.  On this
+                particular slide, we wrote a test so that we can check that when the button is clicked, my Dog Milo's picture is going to show up.
+                Let's take a quick look at the simple code for this slide.
+            </Notes>
         </Slide>
     );
   }
